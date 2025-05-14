1d5f8b36fd010e55b4b56de1f26068f41afc3714
jihodaddy <lastboom@gmail.com>
2025-05-14 11:38:54 +0900

    clean up package

--- backend/src/main/java/com/example/backend/enums/ProductStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.backend.enums;
-
-public enum ProductStatus {
-    ACTIVE, SOLD_OUT, DELETED;
-}
--- backend/src/main/java/com/example/backend/product/enums/ProductStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.product.enums;
-
-public enum ProductStatus {
-    ACTIVE,
-    OUT_OF_STOCK,
-    DELETED
-} 
